262f71c0ade13868619efa630f7d580bb7ec8255
ainazik <47652363+ainazik@users.noreply.github.com>
2019-05-13 14:15:47 +0600

    Update Shape.java

--- project_Java/src/com/company/Shape.java
+++ project_Java/src/com/company/Shape.java
@@ -3,5 +3,14 @@ package com.company;
 import java.awt.*;
 
 enum Shape {
+    ZShape(new int[][]{{0, -1}, {0, 0}, {-1, 0}, {-1, 1}}),
+    SShape(new int[][]{{0, -1}, {0, 0}, {1, 0}, {1, 1}}),
+    IShape(new int[][]{{0, -1}, {0, 0}, {0, 1}, {0, 2}}),
+    TShape(new int[][]{{-1, 0}, {0, 0}, {1, 0}, {0, 1}}),
+    Square(new int[][]{{0, 0}, {1, 0}, {0, 1}, {1, 1}}),
+    LShape(new int[][]{{-1, -1}, {0, -1}, {0, 0}, {0, 1}}),
+    JShape(new int[][]{{1, -1}, {0, -1}, {0, 0}, {0, 1}});
+
+
     
 }
