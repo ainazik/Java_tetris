b7b6bb1375d5f526f1de4e02b0066e49d3b9797f
ainazik <47652363+ainazik@users.noreply.github.com>
2019-05-13 14:25:36 +0600

    Update Tetris.java

--- project_Java/src/com/company/Tetris.java
+++ project_Java/src/com/company/Tetris.java
@@ -345,9 +345,18 @@ public class Tetris extends JPanel implements Runnable {
         for (int[] p : s.pos)
             grid[fallingShapeRow + p[1]][fallingShapeCol + p[0]] = s.ordinal();
     }
-
-
-        });
+    
+        public static void main(String[] args) {
+        SwingUtilities.invokeLater(() -> {
+            JFrame f = new JFrame();
+            f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+            f.setTitle("Tetris");
+            f.setResizable(false);
+            f.add(new Tetris(), BorderLayout.CENTER);
+            f.pack();
+            f.setLocationRelativeTo(null);
+            f.setVisible(true);
+          });
     }
 }
 
