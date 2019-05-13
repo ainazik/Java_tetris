4090507a272878216d8d362e08660060ae7feb81
ainazik <47652363+ainazik@users.noreply.github.com>
2019-05-13 14:09:22 +0600

    Update Scoreboard.java

--- project_Java/src/com/company/Scoreboard.java
+++ project_Java/src/com/company/Scoreboard.java
@@ -1,5 +1,13 @@
 package com.company;
 
 class Scoreboard {
+    static final int MAXLEVEL = 9;
+
+    private int level;
+    private int lines;
+    private int score;
+    private int topscore;
+    private boolean gameOver = true;
+
    
 }
