87211a5e74a72cad4692a6f9edbc528ef74fa4ff
ainazik <47652363+ainazik@users.noreply.github.com>
2019-05-13 14:18:04 +0600

    Update Shape.java

--- project_Java/src/com/company/Shape.java
+++ project_Java/src/com/company/Shape.java
@@ -39,6 +39,23 @@ final class Config {
     final static Rectangle previewRect = new Rectangle(387, 47, 200, 200);
     final static Rectangle titleRect = new Rectangle(100, 85, 252, 100);
     final static Rectangle clickRect = new Rectangle(50, 375, 252, 40);
+    
+    
+    final static int blockSize = 30;
+    final static int nRows = 18;
+    final static int nCols = 12;
+    final static int topMargin = 50;
+    final static int leftMargin = 20;
+    final static int scoreX = 400;
+    final static int scoreY = 330;
+    final static int titleX = 130;
+    final static int titleY = 150;
+    final static int clickX = 120;
+    final static int clickY = 400;
+    final static int previewCenterX = 467;
+    final static int previewCenterY = 97;
+
+
 
 
 
