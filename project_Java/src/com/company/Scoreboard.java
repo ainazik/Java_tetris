b9c97deb372925668515b1b20386306f1adf24e4
ainazik <47652363+ainazik@users.noreply.github.com>
2019-05-13 14:30:42 +0600

    Update Scoreboard.java

--- project_Java/src/com/company/Scoreboard.java
+++ project_Java/src/com/company/Scoreboard.java
@@ -65,6 +65,48 @@ class Scoreboard {
      void addScore(int sc) {
         score += sc;
     }
+    
+    
+    void addLines(int line) {
+
+        switch (line) {
+            case 1:
+                addScore(10);
+                break;
+            case 2:
+                addScore(20);
+                break;
+            case 3:
+                addScore(30);
+                break;
+            case 4:
+                addScore(40);
+                break;
+            default:
+                return;
+        }
+        
+        lines += line;
+        if (lines > 10)
+            addLevel();
+    }
+
+    void addLevel() {
+        lines %= 10;
+        if (level < MAXLEVEL)
+            level++;
+    }
+    int getLevel() {
+        return level;
+    }
+
+    int getLines() {
+        return lines;
+    }
+
+    int getScore() {
+        return score;
+    }
 
 
     
