cdfb331fec6a1ba7e65756cc7a873b954ca799cd
ainazik <47652363+ainazik@users.noreply.github.com>
2019-05-13 14:16:15 +0600

    Update Shape.java

--- project_Java/src/com/company/Shape.java
+++ project_Java/src/com/company/Shape.java
@@ -10,6 +10,23 @@ enum Shape {
     Square(new int[][]{{0, 0}, {1, 0}, {0, 1}, {1, 1}}),
     LShape(new int[][]{{-1, -1}, {0, -1}, {0, 0}, {0, 1}}),
     JShape(new int[][]{{1, -1}, {0, -1}, {0, 0}, {0, 1}});
+    
+        private Shape(int[][] shape) {
+        this.shape = shape;
+        pos = new int[4][2];
+        reset();
+    }
+
+    void reset() {
+        for (int i = 0; i < pos.length; i++) {
+            pos[i] = shape[i].clone();
+        }
+    }
+
+    final int[][] pos, shape;
+}
+
+
 
 
     
