f75d599dbb0a37a8f2718b0d2fdcb5dbc9ce45ab
ainazik <47652363+ainazik@users.noreply.github.com>
2019-05-15 13:59:45 +0600

    Update Shape.java

--- project_Java/src/com/company/Shape.java
+++ project_Java/src/com/company/Shape.java
@@ -24,8 +24,24 @@ enum Shape {
     }
 
     final int[][] pos, shape;
+   
 }
 
+ final class Config {
+    final static Color[] colors = {Color.green, Color.red, Color.blue,
+            Color.pink, Color.orange, Color.cyan, Color.magenta};
+
+    final static Font mainFont = new Font("Monospaced", Font.BOLD, 48);
+    final static Font smallFont = mainFont.deriveFont(Font.BOLD, 18);
+
+    final static Dimension dim = new Dimension(640, 640);
+
+    final static Rectangle gridRect = new Rectangle(46, 47, 308, 517);
+    final static Rectangle previewRect = new Rectangle(387, 47, 200, 200);
+    final static Rectangle titleRect = new Rectangle(100, 85, 252, 100);
+    final static Rectangle clickRect = new Rectangle(50, 375, 252, 40);
+    
+    
 
    
 }
